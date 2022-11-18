a57b0a3df85a355610c9dd72384039baa0a528c8
oldjunping <oldjun@sina.com>
2022-11-18 18:29:18 +0800

    c tiny c++ stl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <array>
 using namespace std;
 
 #include "Vector.h"
@@ -8,25 +8,26 @@ using namespace yazi::stl;
 
 #include <sys/time.h>
 
+
 int main()
 {
+    Vector<int> a;
+    return 0;
+
+    /*
     struct timeval tv;
     gettimeofday(&tv, NULL);
     int start_sec = tv.tv_sec;
     int start_usec = tv.tv_usec;
 
-    const int max = 10000;
-    Vector<bool> v;
-    for (int i = 0; i < max; i++)
-    {
-        v.insert(v.begin(), true);
-    }
+    const int max = 100000;
+    Array<bool> a(max);
 
     gettimeofday(&tv, NULL);
     int end_sec = tv.tv_sec;
     int end_usec = tv.tv_usec;
     double time_diff = (end_sec - start_sec) * 1000000 + (end_usec - start_usec);
     std::cout << time_diff / 1000 / max << "ms" << std::endl;
-
     return 0;
+    */
 }
